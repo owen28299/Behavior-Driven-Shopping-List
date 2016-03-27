51161bc2017d52236654d0623f35edc08d594378
Owen <owenyangg@gmail.com>
2016-03-26 20:48:55 -1000

    refactored list items using flexbox and added bin icon

--- public/js/shopping_list_item.js
+++ public/js/shopping_list_item.js
@@ -14,10 +14,12 @@ function ShoppingListItem(name, description){
   this.render = function(index){
     var output =
       "<li class=" + " \"completed_" + this.is_done + "\"" + ">" +
-        "<h3 class=\"list-name\">" + this.name + "</h3>" +
         "<div class=\"checkboxdiv\"><input type=\"checkbox\" value=\"off\" class=\"checkbox\" data-index=\"" + index + "\">" + "</div>" +
+        "<div class=\"list-text\">" +
+        "<h3 class=\"list-name\">" + this.name + "</h3>" +
         "<p class=\"list-description\">" + this.description + "</p>" +
-        "<div class=\"deletediv\"><button class=\"delete\" data-index=\"" + index +"\">x</button>" + "</div>" +
+        "</div>" +
+        "<div class=\"deletediv\"><input type=image src=\"../img/trash.png\" class=\"delete\" data-index=\"" + index +"\">" + "</div>" +
       "</li>";
 
       return output;
